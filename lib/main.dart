109e0adfde415708efcb70112049927f5d3362c3
IAMTHEMACHINE33 <iamthemachine33@gmail.com>
2022-10-19 15:47:14 +0545

    Day 10 commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Flutter Demo',
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
+    ),
+    home: const Homepage(),
+  ));
+}
+
+class Homepage extends StatelessWidget {
+  const Homepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Register'),
+      ),
+      body: Center(child: TextButton(
+        child: Text("Login"),
+        onPressed: ()async{},
+        )),
+    );
+  }
+}
